be918b64db8df279234ad45ef444ba8c4f2eee8a
zhouhuajian <zhouhuajian@email.com>
2022-05-11 13:26:14 +0800

    播放器主界面[1]

---
+++ main.py
@@ -0,0 +1,17 @@
+"""
+图片播放器 入口文件
+
+@author: zhouhuajian
+@version: v1.0
+"""
+from PyQt6.QtWidgets import QApplication
+
+from player_main_window import PlayerMainWindow
+
+if __name__ == '__main__':
+    # 应用程序
+    app = QApplication([])
+    mainWindow = PlayerMainWindow()
+    mainWindow.show()
+    # 进入消息循环
+    app.exec()
---
+++ player_main_window.py
@@ -0,0 +1,20 @@
+"""
+播放器主窗口
+
+@author: zhouhuajain
+@versoin: v1.0
+"""
+from PyQt6.QtWidgets import QWidget
+
+
+class PlayerMainWindow(QWidget):
+    """播放器主窗口"""
+
+    def __init__(self):
+        """初始化"""
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        """初始化UI"""
+        pass
