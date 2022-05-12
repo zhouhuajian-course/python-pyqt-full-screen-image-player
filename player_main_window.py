27351e69952b03087f3a6f2a6fa8245e70c8dd75
zhouhuajian <zhouhuajian@email.com>
2022-05-12 15:40:04 +0800

    播放器主界面[2]

--- player_main_window.py
+++ player_main_window.py
@@ -4,7 +4,9 @@
 @author: zhouhuajain
 @versoin: v1.0
 """
-from PyQt6.QtWidgets import QWidget
+from PyQt6.QtCore import Qt
+from PyQt6.QtGui import QIcon
+from PyQt6.QtWidgets import QWidget, QPushButton, QHBoxLayout
 
 
 class PlayerMainWindow(QWidget):
@@ -17,4 +19,17 @@ class PlayerMainWindow(QWidget):
 
     def initUI(self):
         """初始化UI"""
-        pass
+        # 设置标题栏
+        self.setWindowFlags(Qt.WindowType.Window |
+                            Qt.WindowType.WindowTitleHint |
+                            Qt.WindowType.WindowCloseButtonHint)
+        self.setWindowTitle("全屏图片播放器")
+        self.setWindowIcon(QIcon("images/window_icon.png"))
+        # 设置窗口大小
+        self.setFixedSize(260, 100)
+        # 打开文件夹按钮
+        mainLayout = QHBoxLayout()
+        self.setLayout(mainLayout)
+        openFolderButton = QPushButton(QIcon("images/folder_icon.png"), "打开文件夹")
+        openFolderButton.setFixedWidth(110)
+        mainLayout.addWidget(openFolderButton)
