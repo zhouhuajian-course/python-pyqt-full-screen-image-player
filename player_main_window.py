2671c06e1835cc486f8e081ba4d61e591a9fe0b8
zhouhuajian <zhouhuajian@email.com>
2022-05-13 18:09:21 +0800

    打开文件夹

--- player_main_window.py
+++ player_main_window.py
@@ -4,13 +4,17 @@
 @author: zhouhuajain
 @versoin: v1.0
 """
+import os
+
 from PyQt6.QtCore import Qt
 from PyQt6.QtGui import QIcon
-from PyQt6.QtWidgets import QWidget, QPushButton, QHBoxLayout
+from PyQt6.QtWidgets import QWidget, QPushButton, QHBoxLayout, QFileDialog
 
 
 class PlayerMainWindow(QWidget):
     """播放器主窗口"""
+    # 常见的图片后缀
+    IMAGE_EXTENSIONS = {".JPG", ".JPEG", ".PNG", ".WEBP", ".BMP"}
 
     def __init__(self):
         """初始化"""
@@ -30,6 +34,31 @@ class PlayerMainWindow(QWidget):
         # 打开文件夹按钮
         mainLayout = QHBoxLayout()
         self.setLayout(mainLayout)
-        openFolderButton = QPushButton(QIcon("images/folder_icon.png"), "打开文件夹")
+        openFolderButton = QPushButton(QIcon("images/folder_icon.png"), "打开文件夹", clicked=self.openFolder)
+        # openFolderButton.clicked.connect()
         openFolderButton.setFixedWidth(110)
         mainLayout.addWidget(openFolderButton)
+
+    def openFolder(self):
+        """打开文件夹"""
+        # print("点击了打开文件夹")
+        # 选择文件夹
+        dirPath = QFileDialog.getExistingDirectory(parent=self)
+        # print(dirPath)
+        # print(repr(dirPath))
+        # 如果用户没有选择文件夹，不做处理
+        if not dirPath:
+            return
+        # 如果用户选择了文件夹，那么遍历文件夹里面的文件，获取图片文件的绝对路径
+        filenameList = os.listdir(dirPath)
+        allImagePaths = []
+        for filename in filenameList:
+            # 过滤非图片文件 .jpg .png
+            fileExt = os.path.splitext(filename)[1]
+            if (not fileExt) or (fileExt.upper() not in self.IMAGE_EXTENSIONS):
+                continue
+            imagePath = os.path.realpath(os.path.join(dirPath, filename))
+            allImagePaths.append(imagePath)
+
+        print(filenameList)
+        print(allImagePaths)
