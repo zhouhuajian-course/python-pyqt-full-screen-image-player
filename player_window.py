7b8d99c1298e5d0b10814adf733ac5f47b60b896
zhouhuajian <zhouhuajian@email.com>
2022-05-19 11:19:28 +0800

    图片播放[2]

--- player_window.py
+++ player_window.py
@@ -5,6 +5,7 @@
 @version: v1.0
 """
 from PyQt6.QtCore import Qt
+from PyQt6.QtGui import QPixmap
 from PyQt6.QtWidgets import QLabel
 
 
@@ -16,10 +17,19 @@ class PlayerWindow(QLabel):
         super().__init__(parent=playerMainWindow)
         self.setWindowFlag(Qt.WindowType.Window)
         self.playerMainWindow = playerMainWindow
+        # 获取屏幕大小
+        self.screenSize = self.screen().size()
+        # 图片居中显示
+        self.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
     def play(self, allImagePaths: list):
         """播放图片"""
         print("正在播放图片...")
-        self.show()
+        # 调试 实现播放第一张图片
+        pixmap = QPixmap(allImagePaths[0]).scaled(self.screenSize, Qt.AspectRatioMode.KeepAspectRatio)
+        self.setPixmap(pixmap)
+        # 隐藏主界面 全屏显示播放器
+        self.playerMainWindow.hide()
+        self.showFullScreen()
         print(allImagePaths)
 
